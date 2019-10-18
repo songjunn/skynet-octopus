b875c1ca685aaff59d3b958cb809414e815fb3ba
songjunn <songjun0810@163.com>
2019-10-18 22:08:40 +0800

    snlua

--- service-src/service_snlua.c
+++ service-src/service_snlua.c
@@ -95,6 +95,7 @@ static void * lalloc(void * ud, void *ptr, size_t osize, size_t nsize) {
             return NULL;
         }
     }
+
     if (l->mem > l->mem_report) {
         l->mem_report *= 2;
         skynet_logger_error(ctx->handle, "[snlua]Memory warning %.2f M", (float)l->mem / (1024 * 1024));
@@ -144,6 +145,7 @@ int snlua_create(struct skynet_service * ctx, int harbor, const char * args) {
         skynet_logger_error(ctx->handle, "[snlua]Load error:%s", lua_tostring(l->L, -1));
         return ret;
     }
+
     lua_getglobal(l->L, "create");  
     lua_pushinteger(l->L, harbor);
     lua_pushinteger(l->L, ctx->handle);
@@ -160,17 +162,11 @@ int snlua_create(struct skynet_service * ctx, int harbor, const char * args) {
 void snlua_release(struct skynet_service * ctx) {
     struct snlua * l = (struct snlua *) ctx->hook;
 
-    int ret = luaL_dofile(l->L, l->mainfile);
+    lua_getglobal(l->L, "release");  
+    int ret = lua_pcall(l->L, 0, 0, 0);
     if (ret != LUA_OK) {
-        skynet_logger_error(ctx->handle, "[snlua]Load error:%s", lua_tostring(l->L, -1));
-    } else {
-        lua_getglobal(l->L, "release");  
-        ret = lua_pcall(l->L, 0, 0, 0);
-        if (ret != LUA_OK) {
-            skynet_logger_error(ctx->handle, "[snlua]Runtime error:%s", lua_tostring(l->L, -1));
-        }
+        skynet_logger_error(ctx->handle, "[snlua]Runtime error:%s", lua_tostring(l->L, -1));
     }
-
     lua_settop(l->L, 0);
     lua_close(l->L);
     skynet_free(l);
@@ -180,20 +176,15 @@ int snlua_callback(struct skynet_service * ctx, uint32_t source, uint32_t sessio
     struct snlua * l = (struct snlua *) ctx->hook;
 
     lua_gc(l->L, LUA_GCSTOP, 0);
-    int ret = luaL_dofile(l->L, l->mainfile);
+    lua_getglobal(l->L, "handle");  
+    lua_pushinteger(l->L, ctx->handle);
+    lua_pushinteger(l->L, source);
+    lua_pushinteger(l->L, session);
+    lua_pushinteger(l->L, type);
+    lua_pushlstring(l->L, msg, sz);
+    int ret = lua_pcall(l->L, 5, 0, 0);
     if (ret != LUA_OK) {
-        skynet_logger_error(ctx->handle, "[snlua]Load error:%s", lua_tostring(l->L, -1));
-    } else {
-        lua_getglobal(l->L, "handle");  
-        lua_pushinteger(l->L, ctx->handle);
-        lua_pushinteger(l->L, source);
-        lua_pushinteger(l->L, session);
-        lua_pushinteger(l->L, type);
-        lua_pushlstring(l->L, msg, sz);
-        ret = lua_pcall(l->L, 5, 1, 0);
-        if (ret != LUA_OK) {
-            skynet_logger_error(ctx->handle, "[snlua]Runtime error:%s", lua_tostring(l->L, -1));
-        }
+        skynet_logger_error(ctx->handle, "[snlua]Runtime error:%s", lua_tostring(l->L, -1));
     }
     lua_settop(l->L, 0);
     lua_gc(l->L, LUA_GCRESTART, 0);
