c8b79bb0faf9a6a8da498e56119f7ef63c45e00a
songjunn <songjun0810@163.com>
2020-06-08 01:02:43 +0800

    logger time

--- skynet-src/skynet_logger.c
+++ skynet-src/skynet_logger.c
@@ -49,7 +49,7 @@ void skynet_logger_format_time(char * buffer, size_t size) {
     gettimeofday(&tv,NULL);
 
     snprintf(buffer, size, "[%02d:%02d:%02d %02d:%02d:%02d.%03d] ", newtime->tm_year+1900, 
-        newtime->tm_mon+1, newtime->tm_mday, newtime->tm_hour, newtime->tm_min, newtime->tm_sec, tv.tv_usec/1000);
+        newtime->tm_mon+1, newtime->tm_mday, newtime->tm_hour, newtime->tm_min, newtime->tm_sec, tv.tv_usec);
 }
 
 void skynet_logger_print(uint32_t source, int level, const char * msg, ...) {
