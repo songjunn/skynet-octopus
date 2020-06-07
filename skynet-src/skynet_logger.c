ddfa2652f412879c23f7b4dbc9c00ba155a7b65b
songjunn <songjun0810@163.com>
2020-06-08 01:50:30 +0800

    fix

--- skynet-src/skynet_logger.c
+++ skynet-src/skynet_logger.c
@@ -48,7 +48,7 @@ void skynet_logger_format_time(char * buffer, size_t size) {
     struct timeval tv;
     gettimeofday(&tv,NULL);
 
-    snprintf(buffer, size, "[%02d:%02d:%02d %02d:%02d:%02d.%03d] ", newtime->tm_year+1900, 
+    snprintf(buffer, size, "[%02d:%02d:%02d %02d:%02d:%02d.%06d] ", newtime->tm_year+1900, 
         newtime->tm_mon+1, newtime->tm_mday, newtime->tm_hour, newtime->tm_min, newtime->tm_sec, tv.tv_usec);
 }
 
