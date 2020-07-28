81d99aafab245305eb5f59722556021de1ccab29
songjunn <songjun0810@163.com>
2020-07-28 14:47:37 +0800

    harbor buffer size

--- service-src/service_harbor.c
+++ service-src/service_harbor.c
@@ -4,7 +4,7 @@
 #include "databuffer.h"
 
 #define BACKLOG 32
-#define BUFFER_MAX 20480
+#define BUFFER_MAX 2048000
 #define CLUSTER_ADDR_MAX 32
 #define SERVICE_NAME_MAX 64
 #define HARBOR_CLUSTER_MAX 32
