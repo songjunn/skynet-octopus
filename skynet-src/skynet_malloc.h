0bba8c3df9ea204387813373a9863e983b4cfcf3
songjunn <songjun0810@163.com>
2020-08-24 19:00:10 +0800

    malloc

--- skynet-src/skynet_malloc.h
+++ skynet-src/skynet_malloc.h
@@ -6,15 +6,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#ifdef USE_JEMALLOC
-#define skynet_malloc je_malloc
-#define skynet_realloc je_realloc
-#define skynet_free(ptr) if(ptr) je_free(ptr);
-#else
-#define skynet_malloc malloc
-#define skynet_realloc realloc
-#define skynet_free(ptr) if(ptr) free(ptr);
-#endif
+void * skynet_malloc(size_t sz);
+void * skynet_realloc(void *ptr, size_t sz);
+void skynet_free(void *ptr);
 
 void skynet_malloc_init();
 void skynet_malloc_free();
